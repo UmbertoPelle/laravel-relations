26d8fd801f4df64952330db6ba22df71c0f0e91f
Umberto <umbertopellegrino96@gmail.com>
2020-10-01 16:11:26 +0200

    migrations

---
+++ database/migrations/2020_10_01_135614_add_foreign_keys.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeys extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(){
+      Schema::table('employees', function (Blueprint $table) {
+        $table -> foreign('location_id', 'emp-loc')
+               -> references('id')
+               -> on('locations');
+      });
+
+      Schema::table('employee_task', function (Blueprint $table) {
+        $table -> foreign('employee_id', 'emp-task')
+               -> references('id')
+               -> on('employees');
+
+        $table -> foreign('task_id', 'task-emp')
+               -> references('id')
+               -> on('tasks');
+      });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::table('employees', function (Blueprint $table){
+        $table -> dropForeign('emp-loc');
+      });
+
+      Schema::table('employee_task', function (Blueprint $table){
+        $table -> dropForeign('emp-task');
+        $table -> dropForeign('task-emp');
+      });
+    }
+}
